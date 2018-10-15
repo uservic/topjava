3ddc98824121e44a87f27c18f3f5ceb1dde46183
Victor <vic1.wmail@gmail.com>
2018-10-16 00:39:15 +0400

    hw2 InMemoryMealRepositoryImpl implemented, InMemoryMealRepositoryImpl logging added

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.repository.mock;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -9,7 +12,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -19,6 +24,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal) {
+        log.info("save {}", meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             repository.put(meal.getId(), meal);
@@ -30,16 +36,19 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public void delete(int id) {
+        log.info("delete {}", id);
         repository.remove(id);
     }
 
     @Override
     public Meal get(int id) {
+        log.info("get {}", id);
         return repository.get(id);
     }
 
     @Override
     public Collection<Meal> getAll() {
+        log.info("getAll");
         return repository.values();
     }
 }
